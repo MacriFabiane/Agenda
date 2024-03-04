e99560589eb775a5b39498075401569673ba0a21
MacriFabiane <mariacristinafabiane@gmail.com>
2024-03-03 21:28:20 -0300

    updating the contacts in database

--- config/process.php
+++ config/process.php
@@ -39,8 +39,36 @@
             }
 
         }
+        else if($data["type"] === "edit"){
 
-        // redirect home
+            $name= $data["name"];
+            $phone= $data["phone"];
+            $observations = $data["observations"];
+            $id = $data["id"];
+
+            $query = "UPDATE contacts SET name =:name , phone= :phone, observations =:observations WHERE id= :id ";
+
+            $stmt= $conn->prepare($query);
+
+            $stmt->bindParam(":name", $name);
+            $stmt->bindParam(":phone", $phone);
+            $stmt->bindParam(":observations", $observations);
+            $stmt->bindParam(":id", $id);
+
+            try{
+
+                $stmt->execute();
+                $_SESSION["msg"]= "Contato atualizado com sucesso!";
+         
+             } catch(PDOException $e){
+                 //erro na conexão
+                 $error = $e->getMessage();
+                 echo "Erro: $error";
+             }
+
+        }
+
+        // redirect home depois que enviar o form
         header("Location: " . $BASE_URL . "../index.php");
         
     }//SELEÇÃO DE DADOS
